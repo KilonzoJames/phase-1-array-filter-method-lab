88f80b141d377bdd7efdadc7ca478375a17e9c56
KilonzoJames <jay.kilonzo@gmail.com>
2023-07-09 15:34:31 +0300

    array-filter-method

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const filter = drivers.filter(driver=>driver.toLowerCase()==name.toLowerCase())
+    return filter;
+}
+findMatching(drivers, "Bobby")
+console.log(findMatching(drivers, "Bobby"));
+
+function fuzzyMatch(drivers, name){
+    const filter = drivers.filter(driver=>driver.slice(0,2)==name)
+    return filter
+}
+console.log(fuzzyMatch(drivers, "Sa"));
+
+function matchName(drivers, name){
+    const filter = drivers.filter(driver=>driver.name==="Bobby")
+    return filter
+}
+console.log(matchName(drivers, "Bobby"));
